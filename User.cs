7b1e61cf2d38889c8fd1562e166b398ee6f1d42a
patelojasv <patelojasv@gmail.com>
2017-09-16 18:21:16 -0500

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User : MonoBehaviour {
+
+	private bool iP; //is the player playing
+	private int m; //money
+
+	public bool IsPlaying { //Cost of the building
+		get { return iP;}
+		set { iP = value;}
+	}
+
+	public int money {
+		get{ return m;}
+		set{ m = value;}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
