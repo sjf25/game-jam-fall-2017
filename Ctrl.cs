8bf0054b79735e8aec047c84a39d47f44781cb73
patelojasv <patelojasv@gmail.com>
2017-09-16 19:02:59 -0500

    Add files via upload

---
+++ Ctrl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Ctrl : MonoBehaviour
+
+{
+
+	int distance = 5;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x = position.x - distance;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x = position.x + distance;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y = position.y + distance;
+			this.transform.position = position;
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y = position.y - distance;
+			this.transform.position = position;
+		}
+	}
+}
